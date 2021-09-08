522b490a0460c89a45eddfdd21b6730cb4b40bbb
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-08 09:38:50 +0100

    Update User.java

--- src/main/java/com/http/tp/entities/User.java
+++ src/main/java/com/http/tp/entities/User.java
@@ -1,10 +1,13 @@
 package com.http.tp.entities;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Size;
@@ -31,6 +34,76 @@ private String email;
 @NotNull
 @Size(max = 128)
 private String password;
+
+@OneToOne(fetch = FetchType.LAZY,
+cascade = CascadeType.ALL,
+mappedBy = "user")
+private UserProfile userProfile;
+
+public long getId() {
+	return id;
+}
+
+public void setId(long id) {
+	this.id = id;
+}
+
+public String getFirstName() {
+	return firstName;
+}
+
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+
+public String getLastName() {
+	return lastName;
+}
+
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
+
+public String getEmail() {
+	return email;
+}
+
+public void setEmail(String email) {
+	this.email = email;
+}
+
+public String getPassword() {
+	return password;
+}
+
+public void setPassword(String password) {
+	this.password = password;
+}
+
+public UserProfile getUserProfile() {
+	return userProfile;
+}
+
+public void setUserProfile(UserProfile userProfile) {
+	this.userProfile = userProfile;
+}
+
+public User(long id, @Size(max = 65) String firstName, @Size(max = 65) String lastName,
+		@Email @Size(max = 100) String email, @Size(max = 128) String password, UserProfile userProfile) {
+	this.id = id;
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.email = email;
+	this.password = password;
+	this.userProfile = userProfile;
+}
+
+public User() {
+}
+
+
+
+
 
 
 
